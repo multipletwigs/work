0106dd3b2655adcb0037da9a11c7ad40d347d419
Zach Khong <lappy@Zachs-MacBook-Air.local>
2024-07-20 17:43:39 +0800

    init

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "log"
+    tea "github.com/charmbracelet/bubbletea"
+    "github.com/charmbracelet/huh"
+    "github.com/charmbracelet/lipgloss"
+    "github.com/charmbracelet/bubbles"
+    "github.com/charmbracelet/harmonica"
+)
+
+
+type model struct{}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+        case tea.KeyMsg:
+            switch msg.String(){
+                case "ctrl+c":
+                    return m, tea.Quit
+            }
+    }
+	return m, nil
+}
+
+func (m model) View() string {
+	return "Hello world"
+}
+
+func main() {
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		log.Fatalf("err: %w", err)
+	}
+	defer f.Close()
+
+	p := tea.NewProgram(model{}, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
